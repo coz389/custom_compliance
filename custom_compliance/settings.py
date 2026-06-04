da7a0d2b6e7986d47459d7a703693ff90c89e7ad
coz389 <sushantakumar.patra@cozentus.com>
2026-06-04 18:05:52 +0530

    access token expire time increse during development

--- custom_compliance/settings.py
+++ custom_compliance/settings.py
@@ -161,7 +161,8 @@ AUTH_USER_MODEL = 'users.User'
 
 #JWT Settings (Simple JWT)
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
